1c232dfeeba0286076f348029f51bc4a8028daf2
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-07-10 11:33:26 -0400

    Handle Garima Bank

--- elements_fetcher.py
+++ elements_fetcher.py
@@ -3,6 +3,31 @@ import asyncio
 from playwright.async_api import async_playwright
 import time
 
+import pytz
+from datetime import datetime, timedelta
+import re
+
+def get_nepali_date(offset_days=0):
+    nepal_tz = pytz.timezone('Asia/Kathmandu')
+    today = datetime.now(nepal_tz) - timedelta(days=offset_days)
+    return today.strftime('%Y-%m-%d')
+
+async def load_with_nepali_date(base_url, page):
+    max_attempts = 5
+    for i in range(max_attempts):
+        nepali_date = get_nepali_date(i)
+        url = re.sub(r"yyyy-mm-dd", nepali_date, base_url)
+        print(f"Trying URL: {url}")
+        await page.goto(url, wait_until='domcontentloaded')
+
+        try:
+            print("Searching for 'No record found' to see if we need to go back'")
+            await page.get_by_text('No record found.').wait_for(timeout=3_000)
+            print(f"❌ No table for date {nepali_date}, trying previous day...")
+        except:
+            print(f"✅ Table found for date {nepali_date}")
+            return
+
 async def open_bank_pages(json_file_path):
     """
     Opens all bank forex pages from nepal_banks.json in separate tabs
@@ -39,12 +64,24 @@ async def open_bank_pages(json_file_path):
         async def open_page(bank):
             """Open a single bank's forex page with developer tools"""
             try:
+                # TODO: Handle anti_robot
+                if bank.get('anti_robot', False):
+                    return
+
+                # TODO: Handle parse whole page, I would need to either use the PDF or the whole page and let gemini parse it
+                if bank.get('parse_whole_page', False):
+                    return
+
                 # Create a new page (tab)
                 page = await context.new_page()
 
                 # Navigate to the forex page
-                print(f"Opening {bank['name']} - {bank['forex_page']}")
-                await page.goto(bank['forex_page'], wait_until='domcontentloaded')
+                forex_page = bank['forex_page']
+                print(f"Opening {bank['name']} - {forex_page}")
+                if bank.get('handle_date', False):
+                    await load_with_nepali_date(forex_page, page)
+                else:
+                    await page.goto(forex_page, wait_until='domcontentloaded', timeout=60_000)
                 if 'table' in bank and bank['table'] == True:
                     table = page.locator('css=table')
                     if 'table_index' in bank:
@@ -55,7 +92,7 @@ async def open_bank_pages(json_file_path):
                 elif 'query_selector' in bank:
                     element = await page.wait_for_selector(bank['query_selector'], state='attached')
                     if not element:
-                        raise Exception(f'Could not find {bank['query_selector']} in {bank['forex_page']}')
+                        raise Exception(f'Could not find {bank['query_selector']} in {forex_page}')
                     print(await element.evaluate('el => el.outerHTML'))
                 elif 'api' in bank:
                     api = bank['api']
